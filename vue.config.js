2290ef0f12f407a729ec2e803de199dc89a7438d
VuXuanThanh <vuxuanthanh2k@gmail.com>
2022-04-16 14:59:18 +0700

    [add] configure  SSL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs');
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8080,
+    https: {
+      key: fs.readFileSync('.certs/key.pem'),
+      cert: fs.readFileSync('.certs/cert.pem'),
+    },
+    // hotOnly: true,
+  },
+
 })
